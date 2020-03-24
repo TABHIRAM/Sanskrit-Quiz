1387cd6e82927085b8155f1e3cf2e1b1003a5e51
TABHIRAM <44461959+TABHIRAM@users.noreply.github.com>
2020-03-24 10:36:12 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,14 @@
+var questions = [
+
+    ["Which Indian Academy is promoting dance, drama and music?", "Sahitya Academy", "Lalit Kala Academy", "National School of Drama","Sangeet Academy", "4"],
+    ["The word ‘Sathyameva Jayate’ is taken from ____", "Yajur Veda", "Atharva Veda", "Mundaka Upanishad","Sam Veda", "3"],
+    ["According to the cultural history of India ‘Panchayatan’ is:", " An assembly of elders of village", "A religious sect", "Temple construction style","Functionary of an administration", "3"],
+    [" Dandia’ is a popular dance of", "Punjab", "Gujarat", "Tamil Nadu","Maharashtra", "2"],
+    ["Where is Pushkar Fair held?", "Udaipur", "Jaisalmer", "Jodhpur","Ajmer", "4"],
+    ["Sattriya is a classical dance form of which State?", " Manipur", "Uttar Pradesh", "Assam","Andhra Pradesh", "3"],
+    ["What was the main effect of vedic culture an Indian history ?", "Solidification of race", "Progress of Metaphysics", " Development of Sanskrit","None of these", "3"],
+    ["The Rath Yatra at Puri is celebrated in honour of which Hindu deity?", "Ram", ".Jaganath", "Shiva","Vishnu", "2"],
+    ["Which classical dance form is famous in Tamil Nadu?", " Kuchipudi", "Mohiniattam", "Bharatanatyam","Kathakali", "3"],
+    ["Historic Chandragiri Fort is located in which of the following states?", "Andhra Pradesh", "Karnataka", "Maharashtra","Telangana", "1"],
+
+  ];
